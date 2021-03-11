c2ddb138366abc7443994b50fe66991053329f1b
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-11 16:50:39 -0500

    commit tree 0269ac547d0ae23dfbec134ef5b84ead8eafe235

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,23 +1,34 @@
-import React, { Component } from 'react'
-import BandInput from './BandInput';
-
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import BandInput from "./BandInput";
+import Band from "./Band";
+import { connect } from "react-redux";
 
 class BandsContainer extends Component {
   render() {
     return (
       <div>
-        <BandInput addBand={this.props.addBand}/>
-
+        <BandInput addBand={this.props.addBand} />
+        <ul>
+          {this.props.bands.map((band) => (
+            <Band
+              name={band.name}
+              id={band.id}
+              deleteBand={this.props.deleteBand}
+            />
+          ))}
+        </ul>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = ({ bands }) => ({ bands })
+const mapStateToProps = ({ bands }) => ({ bands });
 
-const mapDispatchToProps = dispatch => ({
-  addBand: name => dispatch({ type: "ADD_BAND", name })
-})
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addBand: (name) => dispatch({ type: "ADD_BAND", name }),
+    deleteBand: (id) => dispatch({ type: "DELETE_BAND", id }),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer);
